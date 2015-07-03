afb595b84baf7a4d655f640658c52dcb9f00fc65
PeterCoolAssHuber <peterhub@techhuberis.private>
2015-07-03 15:35:43 -0400

    Initial Commit

---
+++ TransformerModel/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TransformerModel
+//
+//  Created by PeterCoolAssHuber on 2015-07-03.
+//  Copyright (c) 2015 Peter Huber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
