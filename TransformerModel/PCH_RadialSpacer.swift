5e037179d849aa82f277ff45d2f7428af19989c2
Peter Huber <peterhub@Peters-MacBook-Pro.local>
2015-07-21 12:09:53 -0400

    Fixed some minor things in PCH_RadialSpacer

--- TransformerModel/PCH_RadialSpacer.swift
+++ TransformerModel/PCH_RadialSpacer.swift
@@ -26,6 +26,19 @@ class PCH_RadialSpacer: PCH_Insulation {
     let W: Double
     let T: Double
     
+    /**
+        Designated initializer
+        
+        :param: type The type of insulation used (usually, T-IV)
+        :param: thickness The thickness of the radial spacer
+        :param: width The width of the radial spacer
+        :param: slLength The SL (overall) length of the radial spacer
+        :param: dLength The D ("effective") length of the radial spacer
+        :param: aEnd The A-end (per Weidmann) configuration of the radial spacer
+        :param: bEnd The B-end (per Weidmann) configuration of the radial spacer
+    
+        :returns: A radial spacer
+    */
     init(type:Insulation, thickness:Double, width:Double, slLength:Double, dLength:Double, aEnd:Int = 0, bEnd:Int = 0)
     {
         self.aEnd = aEnd
@@ -35,6 +48,14 @@ class PCH_RadialSpacer: PCH_Insulation {
         self.W = width
         self.T = thickness
         
-        super.init(material: .TIV)
+        super.init(material: type)
+    }
+    
+    /**
+        The weight of a single radial spacer (uses the average dimension of SL and D)
+    */
+    func Weight() -> Double
+    {
+        return super.Weight(area: self.W * self.T, length:(self.SL + self.D) / 2.0)
     }
 }
