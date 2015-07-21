a4677e8a12d06eafed630f2b2c0e345b7d94b00c
PeterCoolAssHuber <peterhub@techhuberis.private>
2015-07-21 10:08:25 -0400

    Decided on design strategy for PCH_WdgTurn, started implementation

--- TransformerModel/PCH_WdgTurn.swift
+++ TransformerModel/PCH_WdgTurn.swift
@@ -8,18 +8,78 @@
 
 import Cocoa
 
-/// This class represents the combination of cables that make up a winding "turn" (ie: this is what the winder has to wind onto the winding mould)
+/// This class represents the combination of cables that make up a winding "turn" (ie: this is what the winder has to wind onto the winding mould). There are a couple of assumptions made in this class. First of all, it is assumed that for some given pattern of radial cable/insulation pairs, those pairs are repeated in 0 or more axial arrays. That is, if a turn is made up of cable1/radialIns1/cable2, then that pattern may be paralleled in axial copies, but those copies MUST be exact copies of the original turn. It is also assumed that any axial insulation between cables will be constant
 
 class PCH_WdgTurn {
     
     /**
-        A two-dimensional array representing the cables that make up the turn. Note that row indices (first index) refer to the cables in the radial direction, with index 0 being closest to the core. Column indices (second index) refer to cables in the axial direction, with 0 being closest to the bottom
+        Struct to define a radial cable/insulation pair
     */
-    var cableArray = [[PCH_WdgCable]]()
+    struct radialCableArrangement {
+        
+        let cable:PCH_WdgCable
+        let radialInsulation:PCH_Insulation?
+        let radialInsulationThickness:Double?
+    }
+    
+    var cableArray = [[radialCableArrangement]]()
+    
+    var radialCables:radialCableArrangement
+    {
+        get
+        {
+            return cableArray[0][0]
+        }
+    }
+
+    let axialInsulation:PCH_Insulation?
+    let axialInsulationThickness:Double
+    let numAxial:Int
     
     /**
-        A two-dimensional array representing the insulation structures in between the cables in cableArray. By definition, this array will have one less index in each direction than cableArray. Entries may be nil.
+        The  dimensions of the turn over the insulation with the x dimension shrunk (x [axial], y [radial])
     */
-    var interCableArray = [[PCH_Insulation?]]()
-
+    var shrunkDimensionOverCover:(axial:Double, radial:Double)
+        {
+        get
+        {
+            let shrunkAxialIns = (self.axialInsulation == nil ? 0.0 : self.axialInsulation!.shrinkageFactor * self.axialInsulationThickness)
+            
+            
+            
+            
+        }
+    }
+    
+    /**
+    The  dimensions of the strand over the insulation with the x dimension unshrunk (x [axial], y [radial])
+    */
+    var unshrunkDimensionOverCover:(axial:Double, radial:Double)
+        {
+        get
+        {
+            
+        }
+    }
+    
+    init(numAxial:Int, axialIns:PCH_Insulation?, axialInsThk:Double, radArrgs:radialCableArrangement...)
+    {
+        ZAssert(radArrgs.count > 0, "You must define at least one radial cable arrangement!")
+        
+        self.numAxial = numAxial
+        self.axialInsulation = axialIns
+        self.axialInsulationThickness = (axialIns == nil ? 0.0 : axialInsThk)
+        
+        for i in 0..<numAxial
+        {
+            cableArray.insert(radArrgs, atIndex: i)
+        }
+    }
+    
+    convenience init(singleCable:PCH_WdgCable)
+    {
+        self.init(numAxial:1, axialIns:nil, axialInsThk:0.0, radArrgs:radialCableArrangement(cable: singleCable, radialInsulation: nil, radialInsulationThickness: nil))
+    }
+    
+    
 }
