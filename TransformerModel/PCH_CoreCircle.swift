64f1d91252b6d9a02f123059121aedda236eb7a4
Peter Huber <peterhub@peters-macbook-pro.local>
2015-08-09 17:20:47 -0400

    Completed the initializer for PCH_CoreCircle

--- TransformerModel/PCH_CoreCircle.swift
+++ TransformerModel/PCH_CoreCircle.swift
@@ -17,9 +17,9 @@ import Cocoa
     - parameter lowTolerance: The lower tolerance on the area, in p.u. Defaults to 0.995
     - parameter highTolerance: The upper tolerance on the final area, in p.u. Defaults to 1.01
 
-    - returns: A tuple where the first element is the diameter, in meters; and the second element is an array of core-step tuples (width, stack) with both elements in mm, that represent half the core.
+    - returns: A tuple where the first element is the diameter, in meters; the second element is an array of core-step tuples (width, stack) with both elements in mm, that represent half the core; the third tuple is an array of ints that represent the locations (steps) that have ducts
 */
-private func OptimizedCoreDiameter(requiredArea:Double, ducts:[Double]?, sheetThickness:Double, lowTolerance:Double = 0.995, highTolerance:Double = 1.01) -> (Double, [(width:Double, stack:Double)])
+private func OptimizedCoreDiameter(requiredArea:Double, ducts:[Double]?, sheetThickness:Double, lowTolerance:Double = 0.995, highTolerance:Double = 1.01) -> (Double, [(width:Double, stack:Double)], [Int])
 {
     // Define a few constants that we use in the algorithm but are kept here for easy editing during testing
     let minStepHt = 10.0 // mm
@@ -37,9 +37,11 @@ private func OptimizedCoreDiameter(requiredArea:Double, ducts:[Double]?, sheetTh
     // get the initial radius of the core and convert to mm
     var R = diameter / 2.0 * 1000.0
     
-    
     // Create an array to hold the final results
     var ws:[(width:Double, stack:Double)]
+    
+    // Create and initialize an array for duct locations
+    var ductLocs:[Int]
     
     // Loop variables 
     
@@ -52,6 +54,8 @@ private func OptimizedCoreDiameter(requiredArea:Double, ducts:[Double]?, sheetTh
     repeat
     {
         var ductThk = 0.0
+        ductLocs = [Int]()
+        
         if let ductArray = ducts
         {
             let ductWidth = ductArray[nextDuctIndex] * 1000.0 // convert to mm
@@ -60,6 +64,7 @@ private func OptimizedCoreDiameter(requiredArea:Double, ducts:[Double]?, sheetTh
             {
                 ductThk = ductThickness / 2.0 // middle duct is effectively split in two
                 nextDuctIndex++
+                ductLocs.append(0)
             }
         }
         
@@ -99,6 +104,7 @@ private func OptimizedCoreDiameter(requiredArea:Double, ducts:[Double]?, sheetTh
                     {
                         ductThk = ductThickness
                         nextDuctIndex++
+                        ductLocs.append(chkWS.count)
                     }
                 }
             }
@@ -162,7 +168,7 @@ private func OptimizedCoreDiameter(requiredArea:Double, ducts:[Double]?, sheetTh
     
     // At this point, we'll have the routine's best guess at a core, so we just return the final values of R and ws
     
-    return (2.0 * R / 1000.0, ws)
+    return (2.0 * R / 1000.0, ws, ductLocs)
 }
 
 /// The circle in which the core leg's cross-section is defined. The core steps are defined here along with any ducts that are in the core.
@@ -187,15 +193,91 @@ class PCH_CoreCircle
         // Using the given parameters, we calculate the required diameter
         let targetArea = voltsPerTurn / (4.44 * targetBmax * frequency)
         
-        // Before trying to optimize the diameter, we'll do a quick and dirty calculation of W/sq.m. to see if we need ducts and if so, how many. We'll consider a 1m long section of the core to make calculations fast and easy. This section will definitely evolve during testing.
+        // Before trying to optimize the diameter, we'll do a quick and dirty calculation of W/sq.m. to see if we need ducts and if so, how many. We'll consider a 1m long section of the core to make calculations fast and easy. This section is currently quite ugly will definitely evolve during testing.
+        let targetWperSqM = 930.0
         let tWt = targetArea * 1 * steelType.density
         let tLoss = tWt * steelType.SpecificLossAtBmax(targetBmax)
         let tDApprox = sqrt(0.66 * targetArea)
-        var tSurface = tDApprox * π
+        let tSurface = tDApprox * π
         
-        var numDucts = 0
+        var ductWidths:[Double]? = nil
         
+        if (tLoss / tSurface > targetWperSqM)
+        {
+            if (tLoss / (tSurface + 2.0 * tDApprox) < targetWperSqM)
+            {
+                // we need one duct, in the center
+                ductWidths = [tDApprox]
+            }
+            else if (tLoss / (tSurface + 4.0 * (tDApprox - 10.0)) < targetWperSqM)
+            {
+                // we need two ducts, on either side of the main step
+                ductWidths = [tDApprox - 10.0]
+            }
+            else if (tLoss / (tSurface + 2.0 * tDApprox + 4.0 * 0.85 * tDApprox) < targetWperSqM)
+            {
+                // go for three ducts
+                ductWidths = [tDApprox, tDApprox * 0.85]
+            }
+            else if (tLoss / (tSurface + 2.0 * tDApprox + 4.0 * 0.85 * tDApprox + 4.0 * 0.70 * tDApprox) < targetWperSqM)
+            {
+                // skip straight to five ducts
+                ductWidths = [tDApprox, tDApprox * 0.85, tDApprox * 0.7]
+            }
+            else
+            {
+                ALog("Cannot add enough ducts to cool this fucking core!")
+            }
+        }
         
+        // set up some vars to get the data from the optimization call
+        var newDiameter:Double
+        var steps:[(width:Double, stack:Double)]
+        var ductLocs:[Int]
+        
+        // Get the optimized core diameter and steps
+        (newDiameter, steps, ductLocs) = OptimizedCoreDiameter(targetArea, ducts: ductWidths, sheetThickness: steelType.thickness)
+        
+        // save the new diameter
+        self.diameter = newDiameter
+        
+        // initialize the array that we'll fill for the core stack
+        var coreStack = [PCH_CoreStep]()
+        
+        // check if the core has a central duct and if so, save that first
+        if (ductLocs.contains(0))
+        {
+            coreStack.append(PCH_CoreDuct(width:steps[0].width))
+        }
+        
+        // Now, for each step, we create a PCH_CoreStep and add it to the array. If there's a duct on the step, add that too.
+        for i in 0..<steps.count
+        {
+            let lamination = PCH_Lamination(steelType:steelType, width:steps[i].width)
+            coreStack.append(PCH_CoreStep(lamination: lamination, stackHeight: steps[i].stack))
+            
+            if (ductLocs.contains(i+1))
+            {
+                coreStack.append(PCH_CoreDuct(width: steps[i].width))
+            }
+        }
+        
+        // At this point, we have half the core in the array. We need to double the steps (with the exception of the center duct, if any).
+        
+        var index = 0
+        if (ductLocs.contains(0))
+        {
+            index = 1
+        }
+        
+        let baseArray = Array(coreStack[index..<coreStack.count])
+        
+        for i in 0..<baseArray.count
+        {
+            coreStack.insert(baseArray[i], atIndex: 0)
+        }
+        
+        self.steps = coreStack
     }
     
     
